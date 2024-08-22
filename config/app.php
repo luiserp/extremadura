b861399a88518eb15623dbf9c0b9298ff9f6db7c
luiserp <luis@taxo.co>
2024-08-22 15:59:39 -0400

    Install telescope

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         App\Providers\NotificationProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
     /*
